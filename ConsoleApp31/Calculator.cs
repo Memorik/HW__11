cbc91aabbd0c3938333df5133b2ba5ecf4e0f6fd
Memorik <70646403+Memorik@users.noreply.github.com>
2020-09-17 11:39:50 +0300

    Add calculator and tests for them
    Add calculator and tests for them

---
+++ ConsoleApp31/Calculator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp31
+{
+    public class Calculator
+    {
+        static void Main(string[] args)
+        {
+            SimpleCalculator simp = new SimpleCalculator();
+            Console.WriteLine("Write num1");
+            int.TryParse(Console.ReadLine(), out int num1);
+            Console.WriteLine("Write num2");
+            int.TryParse(Console.ReadLine(), out int num2);
+            Console.WriteLine("Write operator");
+            char.TryParse(Console.ReadLine(), out char oper);
+            double result=0;
+            if (oper == '+')
+            {
+                result= simp.Plus(num1, num2);
+            }
+            if (oper == '-')
+            {
+                result = simp.Minus(num1, num2);
+            }
+            if (oper == '*')
+            {
+                result = simp.Multiplication(num1, num2);
+
+            }
+            if (oper == '/')
+            {
+                result = simp.Division(num1, num2);
+
+            }
+
+           Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp31/SimpleCalculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp31
+{
+     public class SimpleCalculator
+    {
+        public double Plus(int num1, int num2)
+        {
+            double result=num1+num2;
+
+            return result;
+        }
+
+        public double Minus(int num1, int num2)
+        {
+            double result = num1 - num2;
+
+            return result;
+        }
+        public double Multiplication(int num1, int num2)
+        {
+            double result = num1 * num2;
+
+            return result;
+        }
+        public double Division(int num1, int num2)
+        {
+            double result = num1 / num2;
+
+            return result;
+        }
+    }
+}
